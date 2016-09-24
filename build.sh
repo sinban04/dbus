ebfbe510bea59b37173102c37dc8c2d0df820e35
Injung <sinban04@gmail.com>
2016-09-24 04:36:40 +0000

    build script bug shooting

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #bin/bash
 
-gcc `pkg-config --libs --cflags dbus-1 glib-2.0 dbus-glib-1` dbus_listen.c -o listen
-gcc `pkg-config --libs --cflags dbus-1 glib-2.0 dbus-glib-1` dbus_sender.c -o sender
+gcc `pkg-config --libs --cflags dbus-1 glib-2.0 dbus-glib-1` dbus_listen.c -o listen `pkg-config --libs --cflags dbus-1 glib-2.0 dbus-glib-1` 
+gcc `pkg-config --libs --cflags dbus-1 glib-2.0 dbus-glib-1` dbus_sender.c -o sender `pkg-config --libs --cflags dbus-1 glib-2.0 dbus-glib-1` 
 
 
 
